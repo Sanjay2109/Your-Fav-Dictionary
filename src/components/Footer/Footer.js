e4c44a105ecfb65dab65b55688dea0841d73b9d0
Sanjay2109 <sanjay_n@outlook.com>
2021-11-26 16:38:55 +0530

    first commit

---
+++ src/components/Footer/Footer.js
@@ -0,0 +1,15 @@
+import React from "react";
+import "./Footer.css";
+
+const Footer = () => {
+  return (
+    <div className="footer">
+      <hr style={{ width: "90%", marginTop: 20 }} />
+      <span className="name">
+        Created by <h1 style={{ width: "90%", marginTop: 20 }}>Sanjay</h1>
+      </span>
+    </div>
+  );
+};
+
+export default Footer;
